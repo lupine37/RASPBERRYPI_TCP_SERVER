e80934d8b12d151516b9841c7a7bde7e7e78a460
lupine37 <lupine37@gmail.com>
2019-04-18 11:32:34 -0900

    added a mastercard program

--- client.py
+++ client.py
@@ -22,7 +22,7 @@ class rfidUID:
         self.card_type = list[3]
 
 
-def sqlData(n):
+def select_sqlData(n):
     data = ()
     wronglist = (0, 0, 0, 0)
     c.execute("SELECT * FROM rfidData WHERE UID_NO = :uid_no", {'uid_no': n})
@@ -33,6 +33,67 @@ def sqlData(n):
         return(wronglist)
 
 
+def Add_sqlData(n):
+    c.execute("SELECT UID_NO FROM rfidData WHERE UID_NO = :uid_no",
+              {'uid_no': n})
+    data = c.fetchone()
+    for uid_no in data:
+        if uid_no == n:
+            print("ALREADY EXIST")
+        else:
+            name = input("NAME: ")
+            house_no = input("HOUSE NO: ")
+
+            with conn:
+                c.execute("""INSERT INTO rfidData VALUES
+                           (:house_no, :name, :uid_no,:card_type)""",
+                          {'house_no': house_no, 'name': name,
+                           'uid_no': n, 'card_type': 'ORD'})
+
+
+def Remove_sqlData(n):
+    c.execute("SELECT UID_NO FROM rfidData WHERE UID_NO = :uid_no",
+              {'uid_no': n})
+    data = c.fetchone()
+    for uid_no in data:
+        if uid_no == n:
+            with conn:
+                c.execute("""DELETE FROM rfidData WHERE UID_no = :uid_no""",
+                          {'uid_no': n})
+
+
+def Mastermode():
+    cardUID = ""
+    response = input("ARE YOU SURE FOR MASTERMODE?(yes/no) ")
+    if response == 'yes':
+        while True:
+            response = input("TO ADD OR REMOVE A CARD OR EXIT: ")
+            if response == 'ADD':
+                print("PLACE YOUR CARD")
+                while True:
+                    cardUID = s.recv(1024).decode('utf-8')
+                    if not cardUID:
+                        break
+                    if cardUID != " ":
+                        print(cardUID)
+                        Add_sqlData(cardUID)
+                        break
+            elif response == 'REMOVE':
+                print("PLACE YOUR CARD")
+                while True:
+                    cardUID = s.recv(1024).decode('utf-8')
+                    if not cardUID:
+                        break
+                    if cardUID != " ":
+                        print(cardUID)
+                        Remove_sqlData(cardUID)
+                        break
+            elif response == 'EXIT':
+                break
+    elif response == 'no':
+        print("ok")
+
+
 def Main():
     count = 0
     while True:
@@ -41,21 +102,24 @@ def Main():
             break
         if (clientData != " "):
             print(clientData)
-            sqldata = sqlData(clientData)
+            sqldata = select_sqlData(clientData)
             try:
                 dataInfo = rfidUID(sqldata)
             except Exception:
                 print("wrong card")
             print(dataInfo.name)
             if dataInfo.uid_no == clientData:
-                count = count + 1
+                if dataInfo.card_type == "MASTERCARD":
+                    Mastermode()
+                elif dataInfo.card_type == "ORD":
+                    count = count + 1
             else:
                 count = count - 1
-            if count == 5:
+            if count == 3:
                 print(Access)
                 s.send(Access.encode('utf-8'))
                 count = 0
-            elif count == -5:
+            elif count == -3:
                 print(Denied)
                 s.send(Denied.encode('utf-8'))
                 count = 0
