13c3826e4ba7857620aea2fa0d87b09b8f41f53a
lupine37 <lupine37@gmail.com>
2019-08-19 17:12:49 +0300

    a working multi_connection server

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from Server import runServer
+import Server
+message = "hi"
 
 
 def Main():
+    recv_data = ""
     while True:
-        runServer()
+        ipAddr = ()
+        Server.accept_wrapper()
+        recv_data = Server.recvData()
+        if recv_data is not None:
+            data = recv_data[0]
+            sock = recv_data[1]
+            ipAddr = recv_data[2]
+            Server.sendData(message, sock, ipAddr)
 
 
 if __name__ == '__main__':
